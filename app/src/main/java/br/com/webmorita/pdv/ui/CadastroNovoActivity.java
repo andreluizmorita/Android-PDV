a6776dd97b7507841000898072e3ce97c0f7d84b
André Morita <andreluizmorita@gmail.com>
2016-03-05 16:24:44 -0300

    Finalizada tela de cadastro

--- app/src/main/java/br/com/webmorita/pdv/ui/CadastroNovoActivity.java
+++ app/src/main/java/br/com/webmorita/pdv/ui/CadastroNovoActivity.java
@@ -1,15 +1,41 @@
 package br.com.webmorita.pdv.ui;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
 
 import br.com.webmorita.pdv.R;
+import br.com.webmorita.pdv.domain.model.Produto;
+import br.com.webmorita.pdv.domain.util.ImageInputHelper;
+import butterknife.Bind;
+import butterknife.OnClick;
 
-public class CadastroNovoActivity extends AppCompatActivity {
+public class CadastroNovoActivity extends BaseActivity implements ImageInputHelper.ImageActionListener {
+
+    @Bind(R.id.editTextDescricao) EditText editTextDescricao;
+    @Bind(R.id.editTextUnidade) EditText editTextUnidade;
+    @Bind(R.id.editTextPreco) EditText editTextPreco;
+    @Bind(R.id.editTextCodigo) EditText editTextCodigo;
+
+    @Bind(R.id.imageViewFoto) ImageView imageViewFoto;
+    @Bind(R.id.imageButtonCamera) ImageButton imageButtonCamera;
+    @Bind(R.id.imageButtonGaleria) ImageButton imageButtonGaleria;
+
+    private ImageInputHelper imageInputHelper;
+
+    private Produto produto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +44,80 @@ public class CadastroNovoActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        imageInputHelper = new ImageInputHelper(this);
+        imageInputHelper.setImageActionListener(this);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+
+                produto = new Produto();
+                produto.setId(0L);
+                produto.setDescricao(editTextDescricao.getText().toString());
+                produto.setUnidade(editTextUnidade.getText().toString());
+
+                if(!editTextPreco.getText().toString().equals("")){
+                    produto.setPreco(Double.parseDouble(editTextPreco.getText().toString()));
+                }else{
+                    produto.setPreco(0.0);
+                }
+
+                produto.setFoto("");
+                produto.save();
+
+                Snackbar.make(view,  "Produto "+ editTextDescricao.getText().toString()+" cadastro com sucesso" , Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                finish();
+
+//                Snackbar.make(view,  editTextUnidade.getText().toString() , Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
+    }
+
+    @OnClick(R.id.imageButtonGaleria)
+    public void onClickGaleria(){
+        imageInputHelper.selectImageFromGallery();
+    }
+
+    @OnClick(R.id.imageButtonCamera)
+    public void onClickCamera(){
+        imageInputHelper.takePhotoWithCamera();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        imageInputHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onImageSelectedFromGallery(Uri uri, File imageFile) {
+        // cropping the selected image. crop intent will have aspect ratio 16/9 and result image
+        // will have size 800x450
+        imageInputHelper.requestCropImage(uri, 100, 100, 0, 0);
+    }
+
+    @Override
+    public void onImageTakenFromCamera(Uri uri, File imageFile) {
+        // cropping the taken photo. crop intent will have aspect ratio 16/9 and result image
+        // will have size 800x450
+        imageInputHelper.requestCropImage(uri, 100, 100, 0, 0);
+    }
+
+    @Override
+    public void onImageCropped(Uri uri, File imageFile) {
+        try {
+            // getting bitmap from uri
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+
+            imageViewFoto.setImageBitmap(bitmap);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
