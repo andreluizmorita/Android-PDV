d6edc2615f5d84d75abf18501192563a909aef3e
André Morita <andreluizmorita@gmail.com>
2016-03-05 10:42:29 -0300

    Criada tela de cadastro

--- app/src/main/java/br/com/webmorita/pdv/ui/MainActivity.java
+++ app/src/main/java/br/com/webmorita/pdv/ui/MainActivity.java
@@ -1,13 +1,17 @@
-package br.com.webmorita.pdv;
+package br.com.webmorita.pdv.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import br.com.webmorita.pdv.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,6 +48,16 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        }else if (id == R.id.action_novo) {
+
+            Intent intent = new Intent(MainActivity.this, CadastroNovoActivity.class);
+
+            startActivity(intent);
+
+        }else if (id == R.id.action_edit) {
+            Log.d("MainActivity","Selecionou editar produto");
+
             return true;
         }
 
